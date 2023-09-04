1ab6eb2a0248e908318ae97d5214dcd7ee630c22
Pai <paaai@icloud.com>
2023-09-03 19:32:50 -0600

    Routing working as intended. Need to work on detail page stilyng, Tomorrow :)

--- src/components/cardContainer.jsx
+++ src/components/cardContainer.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Grid } from '@mui/material';
-import CarCard from './CarCard'; // Make sure the import matches your file name
+import CarCard from './CarCard';
 import { Link } from 'react-router-dom'
 
 
